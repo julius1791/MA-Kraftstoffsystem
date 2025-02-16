d8d239599f52fdec571333976f9d27ef0ed0f5cb
Julius <julius.sessler@rwth-aachen.de>
2025-02-17 00:19:47 +0100

    commit before major change

--- fuelsystem.py
+++ fuelsystem.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+
 import fuelflow
-import h2_stoffmodell
 from parallel_comustion import calc_parallel_combustion
 
 tolerance = 1e-6
+max_iter = 200
 v0 = 40
 
 def reference(t_cb, qm_hpfp, Q_fohe, Q_idg, p_hpfp, eta_hpfp, p_lpfp, eta_lpfp, qm_cb, t0, p0, v = v0, tolerance = tolerance):
@@ -25,6 +26,8 @@ def reference(t_cb, qm_hpfp, Q_fohe, Q_idg, p_hpfp, eta_hpfp, p_lpfp, eta_lpfp, 
         jetaflow.split_flows(qm_t)
         t_r1 = jetaflow.t
         qm_t = qm_t + (t_r1 - t_cb)/300
+        if i > max_iter:
+            return 0, 0, 0, 0, 200
     qm_r = qm_hpfp - qm_t - qm_cb
     return qm_r, qm_t, P_lpfp, P_hpfp, i
 
@@ -65,12 +68,15 @@ def h2pump(t_cbt, t_hxt, eta_hpfp, eta_r, p_cbt, qm_cb0, t0, p0, Q_hx = 0, pcc=F
         p_cba = ff_cb.p
         p_hpfp += p_cbt - p_cba
         h_r = h2flow.calc_h()
-        qm_r += (t_hxt-t_hxa)/400
+        qm_r += (t_hxt-t_hxa)/200
+        print(i, abs(t_cbt - t_cba), abs(t_hxa-t_hxt), abs(p_cbt - p_cba), abs(h_r - h_rold))
         condition_bool = (
             abs(t_cbt - t_cba) > tolerance 
             and abs(t_hxa-t_hxt) > tolerance 
             and abs(p_cbt - p_cba) > tolerance 
             and abs(h_r - h_rold) > tolerance) 
+        if i > max_iter:
+            return 0, 0, 0, 0, 200
     print(t_cbt - t_cba, t_hxa-t_hxt, p_cbt - p_cba, h_r - h_rold)
     return qm_r, P_hpfp, P_r, dH-P_hpfp, i
 
@@ -112,6 +118,8 @@ def h2after(t_cbt, t_hxt, eta_hpfp, eta_r, p_cbt, qm_cb0, t0, p0, Q_hx = 0, pcc=
             and abs(t_hxa-t_hxt) > tolerance 
             and abs(p_cbt - p_cba) > tolerance 
             and abs(h_r - h_rold) > tolerance) 
+        if i > max_iter:
+            return 0, 0, 0, 0, 200
     print(t_cbt - t_cba, t_hxa-t_hxt, p_cbt - p_cba, h_r - h_rold)
     return qm_r, P_hpfp, P_r, dH-P_hpfp, i
 
@@ -165,6 +173,10 @@ def h2dual(t_cbt, t_hxt, eta_hpfp, eta_r, p_cbt, qm_cb0, t0, p0, Q_hx = 0, pcc=F
             and abs(p_cbt - p_cba) > tolerance 
             and abs(h_r - h_rold) > tolerance
             and abs(h_v - h_vold) > tolerance)
+        if i > max_iter:
+            return 0, 0, 0, 0, 200
+        if i > max_iter:
+            return 0, 0, 0, 0, 0, 200
     print(t_cbt - t_cba, t_hxa-t_hxt, p_cbt - p_cba, h_r - h_rold)
     return qm_r, qm_v, P_hpfp, P_r, dH-P_hpfp, i
 
@@ -204,6 +216,8 @@ def h2pre(t_cbt, t_hxt, eta_hpfp, p_cbt, qm_cb0, t0, p0, Q_hx = 0, pcc=False, v 
             and abs(t_hxa-t_hxt) > tolerance 
             and abs(p_cbt - p_cba) > tolerance 
             and abs(h_r - h_rold) > tolerance) 
+        if i > max_iter:
+            return 0, 0, 0, 0, 200
     print(t_cbt - t_cba, t_hxa-t_hxt, p_cbt - p_cba, h_r - h_rold)
     return qm_r, P_hpfp, dH-P_hpfp, i
 
@@ -213,8 +227,8 @@ def h2pre(t_cbt, t_hxt, eta_hpfp, p_cbt, qm_cb0, t0, p0, Q_hx = 0, pcc=False, v 
 
 if __name__ == "__main__":
 
-    t_bk = 300
-    t_wu = 200
+    t_bk = 400
+    t_wu = 250
     eta_p = 0.88
     eta_r = 0.8
     p_cb = 1.5e6
@@ -223,35 +237,35 @@ if __name__ == "__main__":
     p0 = 4.2e5
     
     
-    print("reference")
-    print("qmr qmt Plp Php i")
-    qm_r , qm_t, P_lpfp, P_hpfp, i = reference(430, 1, 200000, 5500, p_cb, 0.88, 5e5, 0.83, 0.3, 250, 0.4e5)
-    print(round(qm_r, 4) , round(qm_t, 4), round(P_lpfp/1000, 3), round(P_hpfp/1000, 3), i)
+    # print("reference")
+    # print("qmr qmt Plp Php i")
+    # qm_r , qm_t, P_lpfp, P_hpfp, i = reference(430, 1, 200000, 5500, p_cb, 0.88, 5e5, 0.83, 0.3, 250, 0.4e5)
+    # print(round(qm_r, 4) , round(qm_t, 4), round(P_lpfp/1000, 3), round(P_hpfp/1000, 3), i)
     
-    print("\nh2dual")
-    print("qmr qmv Php P_r Q i")
-    qm_r1, qm_v, P_hpfp, P_r, Q, i = h2dual(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0)
-    print(round(qm_r1, 4), round(qm_v, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
+    # print("\nh2dual")
+    # print("qmr qmv Php P_r Q i")
+    # qm_r1, qm_v, P_hpfp, P_r, Q, i = h2dual(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0)
+    # print(round(qm_r1, 4), round(qm_v, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
     
     print("\nh2pump")
     print("qmr Php P_r Q i")
     qm_r1, P_hpfp, P_r, Q, i = h2pump(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0)
     print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
     
-    print("\nh2pump_pcc")
-    print("qmr Php P_r Q i")
-    qm_r1, P_hpfp, P_r, Q, i = h2pump(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0, 200e3, True)
-    print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
+    # print("\nh2pump_pcc")
+    # print("qmr Php P_r Q i")
+    # qm_r1, P_hpfp, P_r, Q, i = h2pump(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0, 200e3, True)
+    # print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
     
-    print("\nh2after")
-    print("qmr Php P_r Q i")
-    qm_r1, P_hpfp, P_r, Q, i = h2after(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0)
-    print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
+    # print("\nh2after")
+    # print("qmr Php P_r Q i")
+    # qm_r1, P_hpfp, P_r, Q, i = h2after(t_bk, t_wu, eta_p, eta_r, p_cb, qm_cb, t0, p0)
+    # print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(P_r/1000, 3), round(Q/1000, 3), i)
     
-    print("\nh2pre")
-    print("qmr Php Q i")
-    qm_r1, P_hpfp, Q, i = h2pre(t_bk, t_wu, eta_p, p_cb, qm_cb, t0, p0)
-    print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(Q/1000, 3), i)
+    # print("\nh2pre")
+    # print("qmr Php Q i")
+    # qm_r1, P_hpfp, Q, i = h2pre(t_bk, t_wu, eta_p, p_cb, qm_cb, t0, p0)
+    # print(round(qm_r1, 4), round(P_hpfp/1000, 3), round(Q/1000, 3), i)
 
 
 
